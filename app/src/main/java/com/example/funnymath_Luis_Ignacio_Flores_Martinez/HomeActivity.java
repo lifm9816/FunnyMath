7c569695d3f340001af0cc49d3c076237efe248e
Luis Martínez <lifm9816@gmail.com>
2024-11-06 21:16:51 -0600

    Correcciones visuales

--- app/src/main/java/com/example/funnymath_Luis_Ignacio_Flores_Martinez/HomeActivity.java
+++ app/src/main/java/com/example/funnymath_Luis_Ignacio_Flores_Martinez/HomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.funnymath_Luis_Ignacio_Flores_Martinez;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,18 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class HomeActivity extends AppCompatActivity {
 
+    ImageView mathy_d;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_home);
+
+        mathy_d = findViewById(R.id.mathy_d);
+
+        mathy_d.setImageResource(R.drawable.mathy);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
